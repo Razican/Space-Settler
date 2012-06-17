72f0883e676d435355f5679e05b8ca87d27e9e09
Iban Eguia <admin@razican.com>
2012-06-17 19:16:38 +0200

    Ahora se puede ir a la visión general haciendo click en el logo.

--- skins/default/views/ingame/topbar.php
+++ skins/default/views/ingame/topbar.php
@@ -1,4 +1,4 @@
-<div id="logo"><?php echo img(array('src' => 'images/logo.png', 'alt' => 'Logo')); ?></div>
+<div id="logo"><?php echo anchor('/', img(array('src' => 'images/logo.png', 'alt' => 'Logo'))); ?></div>
 <div id="topbar">
 <?php echo lang('overal.server_time'); ?>: <?php echo show_date(); ?>
 </div>
